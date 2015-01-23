0c8a55be70f7e8d1977c6c4548d56f1dbb00d0ea
Joshua Chi <joshua@lohas.club>
2015-01-23 11:54:10 +0800

    test

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: serve
+web: node index.js
---
+++ index.js
@@ -0,0 +1,48 @@
+// var connect = require('connect')
+// var http = require('http')
+// var express = require("express")
+//
+// var app = connect();
+// app.use("/index", express.static(__dirname + '/index.html'));
+// app.use("/locale.js", express.static(__dirname + '/locale.js'));
+// app.use("/dist/prose.min.js", express.static(__dirname + '/dist/prose.min.js'));
+// // fs = require('fs');
+// //
+// //
+// // fs.readFile('./index.html', function (err, html) {
+// //   if (err) {
+// //     throw err;
+// //   }
+// //   http.createServer(function(request, response) {
+// //       response.writeHeader(200, {"Content-Type": "text/html"});
+// //       response.write(html);
+// //       response.end();
+// //   }).listen(3000);
+// // });
+//
+// // respond to all requests
+// // app.use(function(req, res){
+// //   res.end('Hello from Connect!\n');
+// // })
+//
+// //create node.js http server and listen on port
+// http.createServer(app).listen(3000)
+
+
+var express = require("express"),
+    app = express(),
+    fs = require('fs');
+    
+app.use(express.static(__dirname + '/'));
+app.get('/', function(req, res){
+  fs.readFile('./index.html', function (err, html) {
+    if (err) {
+      throw err;
+    }
+    
+    res.writeHeader(200, {"Content-Type": "text/html"});
+    res.write(html);
+    res.end();
+  });
+});
+app.listen(3000);
